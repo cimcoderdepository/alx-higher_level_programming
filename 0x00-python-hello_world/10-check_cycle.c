b36038556eb8c1624c54f00188295667ba243c29
cimcoderdepository <gloryland4ever@gmail.com>
2023-03-07 07:15:38 +0100

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tail = list;
+	listint_t *head = list;
+
+	if (!list)
+		return (0);
+
+	while (tail && head && head->next)
+	{
+		tail = tail->next;
+		head = head->next->next;
+		if (tail == head)
+			return (1);
+	}
+
+	return (0);
+}
