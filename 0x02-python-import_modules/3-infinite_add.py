4e7f3b159bd7da5483d001d15cf43d4aebeedb2c
cimcoderdepository <gloryland4ever@gmail.com>
2023-03-10 04:42:26 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,11 @@
 import sys
 if __name__ == "__main__":
 
-# Get all the arguments except the first one (which is the script name)
+    # Get all the arguments except the first one (which is the script name)
     arguments = sys.argv[1:]
 
-# Convert each argument to a float and sum them up
+    # Convert each argument to a float and sum them up
     result = sum(map(int, arguments))
 
-# Print the result
+    # Print the result
     print(result)
-
