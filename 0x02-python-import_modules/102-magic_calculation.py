33178b09aa443fd9766a8a4816b5e0b23d0b588e
cimcoderdepository <gloryland4ever@gmail.com>
2023-03-12 00:53:56 +0100

    task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin.python3
 def magic_calculation(a, b):
-    add, sub = __import__("magic_calculation_102", ["add", "sub"], 0)
+    add, sub = __import__("magic_calculation_102", ("add", "sub"), 0)
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
+        for i in range(4, 7):
             c = add(c, i)
         return c
     else:
