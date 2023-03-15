bf1405bd5d4ea9b65c2518c34157125a8dcacc1c
cimcoderdepository <gloryland4ever@gmail.com>
2023-03-15 02:05:28 +0100

    task 10

---
+++ 0x03-python-data_structures/10-disassemble.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import dis
+divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+dis.dis(divisible_by_2)
---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    """
+    Returns a new list with True or False, depending on whether the integer at the same position in the original list is a multiple of 2
+    """
+    result = []
+    for item in my_list:
+        result.append(item % 2 == 0)
+    return result
