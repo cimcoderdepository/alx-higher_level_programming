84cecbbb33339e74d8809678e3aa27dcb81059c8
cimcoderdepository <gloryland4ever@gmail.com>
2023-03-28 23:56:33 +0100

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -11,10 +11,10 @@ class Square:
 
         Attributes:
             __size (int): The size of the square:
-                Note that __size (double leading undercore) is a private instance
-                attribute, indicated by the double underscores before its name.
-                This means that it can only be accessed from within the class and
-                not from outside of it. The size argument passed to the constructor
-                is assigned to this attribute.
+            Note that __size (double leading undercore) is a private instance
+            attribute, indicated by the double underscores before its name.
+            This means that it can only be accessed from within the class and
+            not from outside of it. The size argument passed to the constructor
+            is assigned to this attribute.
         """
         self.__size = size
