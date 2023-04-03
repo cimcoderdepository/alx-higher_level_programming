a385c8e5c34baa975949d0af334865c88adcedfc
cimcoderdepository <gloryland4ever@gmail.com>
2023-04-03 23:24:57 +0100

    commit task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""An empty class Rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """Create an empty class."""
---
+++ 0x08-python-more_classes/robot_main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+from robot_asimov import Robot
+for number, text in enumerate(Robot.Three_Laws):
+    print(str(number+1) + ":\n" + text)
