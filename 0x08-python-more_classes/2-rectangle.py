6e8b6a3fe5977b39358660fb3c0212b3711436a2
cimcoderdepository <gloryland4ever@gmail.com>
2023-04-04 15:43:39 +0100

    task 0

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""This module defines a real rectangle."""
+
+
+class Rectangle:
+    """Create the Rectangle class."""
+
+    def __init__(self, width=0, height=0, area=0, perimeter=0):
+        """
+        Initialize a rectangle.
+
+        Args:
+            width
+            height
+
+        Attribute:
+            __width
+            __height
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        Property getter.
+
+        Args:
+            None
+
+        Return:
+            self.__height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args="value"
+
+        Attributes="height"
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__height = value
+
+    @property
+    def width(self):
+        """
+        Property getter.
+
+        Returns:
+            self.width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args="value"
+
+        Attributes="width"
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = value
+
+        def area(self):
+            self.area = height * width
+            return self.area
+
+        def perimeter(self):
+            if height == 0 or width == 0:
+                self.perimeter = 0
+            return (height + weight) * 2
