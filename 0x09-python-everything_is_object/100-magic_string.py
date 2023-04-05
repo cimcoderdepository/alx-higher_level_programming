283c4a82ceaf8d14b8b7b9853e7f9c46635054b8
cimcoderdepository <gloryland4ever@gmail.com>
2023-04-05 05:38:19 +0100

    task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    print(str="BestSchool")
-    return str * n
+    magic_string.n = getattr(magic_string, "n", 0) + 1
+    return "Bestchool, " * (magic_string.n - 1) + "BestSchool"
